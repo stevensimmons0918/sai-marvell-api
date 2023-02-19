c09bafa3cfc17fb72d5de2654e7dfcf90e4ed3b8
Simmons, Steven <simmonss@localhost>
2023-02-19 12:17:39 -0600

    init coding

--- cpssHal/cpssHalInit/cpssHalDev.c
+++ cpssHal/cpssHalInit/cpssHalDev.c
@@ -767,6 +767,9 @@ PROFILE_STC aldrin2_xl_port_profile[] =
 PROFILE_STC aldrin2_xl_fujitsu_large_profile[] =
 #include "aldrin2_xl_fujitsu_large_profile.h"
 
+PROFILE_STC aldrin2_xl_fujitsu_large_eb3_profile[] =
+#include "aldrin2_xl_fujitsu_large_eb3_profile.h"
+
 PROFILE_STC aldrin2_xl_fujitsu_large_eval_profile[] =
 #include "aldrin2_xl_fujitsu_large_eval_profile.h"
 
