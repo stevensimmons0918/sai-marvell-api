cc1725ce15ae09eadc4b2918032e4ddda17ecb5e
Simmons, Steven <simmonss@localhost>
2023-06-08 10:49:17 -0500

    testing kre

--- cpssHal/cpssHalInit/include/port_profiles/aldrin2_xl_fujitsu_large_eval_profile.h
+++ cpssHal/cpssHalInit/include/port_profiles/aldrin2_xl_fujitsu_large_eval_profile.h
@@ -6,7 +6,7 @@
     {.profileType = PROFILE_TYPE_PORT_MAP_E, .profileValue.portMap = { 44, ALDRIN2_MAC_AND_TXQ_PORT_MAC(44), PROFILE_INTERFACE_MODE_SR_LR_E, 10, .phyInfo = {0, 0, 0, 0}, 44 }},
     {.profileType = PROFILE_TYPE_PORT_MAP_E, .profileValue.portMap = { 45, ALDRIN2_MAC_AND_TXQ_PORT_MAC(45), PROFILE_INTERFACE_MODE_SR_LR_E, 10, .phyInfo = {0, 0, 0, 0}, 45 }},
     {.profileType = PROFILE_TYPE_PORT_MAP_E, .profileValue.portMap = { 46, ALDRIN2_MAC_AND_TXQ_PORT_MAC(46), PROFILE_INTERFACE_MODE_SR_LR_E, 10, .phyInfo = {0, 0, 0, 0}, 46 }},
-    {.profileType = PROFILE_TYPE_PORT_MAP_E, .profileValue.portMap = { 47, ALDRIN2_MAC_AND_TXQ_PORT_MAC(47), PROFILE_INTERFACE_MODE_SR_LR_E, 10, .phyInfo = {0, 0, 0, 0}, 47 }},
+    {.profileType = PROFILE_TYPE_PORT_MAP_E, .profileValue.portMap = { 47, ALDRIN2_MAC_AND_TXQ_PORT_MAC(47), PROFILE_INTERFACE_MODE_KR_E, 10, .phyInfo = {0, 0, 0, 0}, 47 }},
 
     //CPU SDMA PORTs
     {.profileType = PROFILE_TYPE_CPU_PORT_MAP_E, .profileValue.portMap = { 63, 0, 99, PROFILE_INTERFACE_MODE_KR_E, 10 }},
